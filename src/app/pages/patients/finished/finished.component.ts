d37740d384f61811c539c9c5b9f029c650e58114
ruwanliyanage <ruwanliyanagegalle1994@gmail.com>
2019-05-05 18:02:42 +0530

    made table in read only manner

--- src/app/pages/patients/finished/finished.component.ts
+++ src/app/pages/patients/finished/finished.component.ts
@@ -11,9 +11,6 @@ import { ProfileComponent } from '../profile/profile.component';
   template: `
     <ng2-smart-table
       [settings]="settings"
-      (createConfirm)="addData($event)"
-      (editConfirm)="editData($event)"
-      (deleteConfirm)="deleteData($event)"
       (userRowSelect)="onCustomAction($event)"
       [source]="list"
     ></ng2-smart-table>
@@ -39,39 +36,23 @@ export class FinishedComponent implements OnInit {
   }
 
   settings = {
-    add: {
-      addButtonContent: '<i class="nb-plus"></i>',
-      createButtonContent: '<i class="nb-checkmark"></i>',
-      cancelButtonContent: '<i class="nb-close"></i>',
-      confirmCreate: true
-    },
-    edit: {
-      editButtonContent: '<i class="nb-edit"></i>',
-      saveButtonContent: '<i class="nb-checkmark"></i>',
-      cancelButtonContent: '<i class="nb-close"></i>',
-      confirmSave: true
-    },
-    delete: {
-      deleteButtonContent: '<i class="nb-trash"></i>',
-      confirmDelete: true
+    hideSubHeader: true,
+
+    actions: {
+      add: false,
+      edit: false,
+      delete: false
     },
 
     columns: {
       nic: {
-        title: 'NIC'
-      },
-      name: {
-        title: 'Name'
-      },
-      address: {
-        title: 'Address'
+        title: 'NIC',
+        editable: false
       },
       email: {
         title: 'Email'
       },
-      contactNumber: {
-        title: 'Contact Number'
-      },
+
       haircolor: {
         title: 'Hair Color'
       },
@@ -81,69 +62,15 @@ export class FinishedComponent implements OnInit {
       hairstyle: {
         title: 'Hair Style'
       },
-      dob: {
-        title: 'DOB'
-      },
-      gender: {
-        title: 'Gender'
-      },
       level: {
         title: 'Level'
       }
     }
   };
 
-  addData(event) {
-    event.newData.level = 'not-selected';
-    this.list.push(event.newData);
-
-    this.service.addPatient({ data: this.list }).subscribe(next => {
-      event.confirm.reject();
-    });
-  }
-
-  /**
-   * this function for delete table data. after clicking the delete option.
-   * ann event will pass after click the delete button.
-   * find the relavent nic by event.data.nic
-   * then remove the relavent object from the source array
-   * after remain array will store in the firebase.
-   */
-  deleteData(event) {
-    if (window.confirm('Are you sure you want to Delete?')) {
-      this.list = this.list.filter(obj => obj.nic !== event.data.nic);
-      this.service.addPatient({ data: this.list }).subscribe(next => {
-        event.confirm.reject();
-      });
-      event.confirm.resolve(event.newData);
-    } else {
-      event.confirm.reject();
-    }
-  }
-
-  /**
-   * This function for store the changes in firebase
-   * first remove the previous data from the manu list
-   * after add the new changed data into manu array
-   * then manu array will store in the fitrebase
-   */
-  editData(event) {
-    if (window.confirm('Are you sure you want to save Changes?')) {
-      this.list = this.list.filter(obj => obj.nic !== event.data.nic);
-      this.list.push(event.newData);
-      this.service.addPatient({ data: this.list }).subscribe(next => {
-        event.confirm.reject();
-      });
-      event.confirm.resolve(event.newData);
-    } else {
-      event.confirm.reject();
-    }
-  }
   onCustomAction(event) {
     this.isPopupOpened = true;
-
     this.service.setDialog(event.data);
-
     const dialogRef = this.dialog.open(ProfileComponent, {
       data: {}
     });
