83778465a1516481cf071bdeb0d48581a478828d
ruwanliyanage <ruwanliyanagegalle1994@gmail.com>
2019-05-29 10:02:05 +0530

    add handleInput function

--- src/app/pages/design/addposts/addposts.component.ts
+++ src/app/pages/design/addposts/addposts.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
+import { FireserviceService } from '../fireservice.service';
 
 @Component({
-  selector: 'ngx-addposts',
+  selector: 'app-addposts',
   templateUrl: './addposts.component.html',
-  styleUrls: ['./addposts.component.scss']
+  styleUrls: ['./addposts.component.css']
 })
 export class AddpostsComponent implements OnInit {
+  constructor(private firestoreService: FireserviceService) {}
 
-  constructor() { }
+  image: any = null; //list of files
+  percentage: any = this.firestoreService.percentage;
+  public postsForm = new FormGroup({
+    title: new FormControl('', Validators.required),
+    content: new FormControl('', Validators.required),
+    cover: new FormControl('', Validators.required)
+  });
 
-  ngOnInit() {
+  public handleInput($event: Event) {
+    //getting the image or files
+    this.image = $event.target['files'];
+    console.log(this.image);
   }
 
+  //submit
+  public addPost(data: FormData) {
+    this.firestoreService.createPost(data, this.image);
+  }
+
+  ngOnInit() {
+    this.firestoreService.percentageChanges.subscribe(
+      x => (this.percentage = x)
+    );
+  }
 }
