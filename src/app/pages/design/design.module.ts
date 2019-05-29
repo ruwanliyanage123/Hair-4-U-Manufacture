48d6e5cbb7bcb5045d68e676e818807badbd9b4e
ruwanliyanage <ruwanliyanagegalle1994@gmail.com>
2019-05-29 09:34:29 +0530

    add addpost component

--- src/app/pages/design/design.module.ts
+++ src/app/pages/design/design.module.ts
@@ -4,9 +4,10 @@ import { DesignComponent } from './design.component';
 import { DesignRoutingModule } from './design-routing.module';
 import { CreateComponent } from './create/create.component';
 import { ViewComponent } from './view/view.component';
+import { AddpostsComponent } from './addposts/addposts.component';
 
 @NgModule({
-  declarations: [DesignComponent],
+  declarations: [DesignComponent, AddpostsComponent],
   imports: [CommonModule, DesignRoutingModule]
 })
 export class DesignModule {}
