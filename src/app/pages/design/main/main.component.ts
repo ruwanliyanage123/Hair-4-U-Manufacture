f309054bf170262dc6f8da746b739743508768ce
ruwanliyanage <ruwanliyanagegalle1994@gmail.com>
2019-05-29 10:11:32 +0530

    add getPosts() function

--- src/app/pages/design/main/main.component.ts
+++ src/app/pages/design/main/main.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { FireserviceService } from '../fireservice.service';
 
 @Component({
-  selector: 'ngx-main',
+  selector: 'app-main',
   templateUrl: './main.component.html',
-  styleUrls: ['./main.component.scss']
+  styleUrls: ['./main.component.css']
 })
 export class MainComponent implements OnInit {
+  constructor(private firestoreService: FireserviceService) {}
+  posts: any[];
 
-  constructor() { }
-
-  ngOnInit() {
+  getPosts() {
+    this.firestoreService.getPosts().subscribe(posts => {
+      this.posts = [];
+      posts.map(post => {
+        this.posts.push({
+          id: post.payload.doc.id,
+          data: post.payload.doc.data()
+        });
+      });
+      console.log(this.posts);
+    });
   }
 
+  deletePost(id: string, image: string) {
+    console.log(image);
+    console.log(id);
+    this.firestoreService.deletePost(id, image);
+  }
+
+  ngOnInit() {
+    this.getPosts();
+  }
 }
