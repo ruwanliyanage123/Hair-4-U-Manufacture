6472f89d8c4233208a18ecf2cbccd3fdc69157d1
ruwanliyanage <ruwanliyanagegalle1994@gmail.com>
2019-05-29 09:35:33 +0530

    add main component

--- src/app/pages/design/design.module.ts
+++ src/app/pages/design/design.module.ts
@@ -5,9 +5,10 @@ import { DesignRoutingModule } from './design-routing.module';
 import { CreateComponent } from './create/create.component';
 import { ViewComponent } from './view/view.component';
 import { AddpostsComponent } from './addposts/addposts.component';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
-  declarations: [DesignComponent, AddpostsComponent],
+  declarations: [DesignComponent, AddpostsComponent, MainComponent],
   imports: [CommonModule, DesignRoutingModule]
 })
 export class DesignModule {}
