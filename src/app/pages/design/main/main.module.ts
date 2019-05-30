6c5a5c75425d7b8756ca9a212a854af9a1c7ee4c
ruwanliyanage <ruwanliyanagegalle1994@gmail.com>
2019-05-30 10:34:54 +0530

    make a changes in wigs showroom

--- src/app/pages/design/main/main.module.ts
+++ src/app/pages/design/main/main.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainComponent } from './main.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [MainComponent],
-  imports: [CommonModule, FormsModule, ReactiveFormsModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule, MatButtonModule],
   exports: [MainComponent]
 })
 export class MainModule {}
