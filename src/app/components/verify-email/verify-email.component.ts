8a97cc6d7060d3a026efa31d900050027b172b67
ruwanliyanage <ruwanliyanagegalle1994@gmail.com>
2019-05-31 15:29:33 +0530

    add css for the verify-email

--- src/app/components/verify-email/verify-email.component.ts
+++ src/app/components/verify-email/verify-email.component.ts
@@ -4,13 +4,10 @@ import { AuthService } from '../../shared/services/auth.service';
 @Component({
   selector: 'ngx-verify-email',
   templateUrl: './verify-email.component.html',
-  styleUrls: ['./verify-email.component.scss']
+  styleUrls: ['./verify-email.component.css']
 })
 export class VerifyEmailComponent implements OnInit {
+  constructor(public authService: AuthService) {}
 
-  constructor( public authService: AuthService) { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
