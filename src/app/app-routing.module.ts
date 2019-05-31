59c059059eb511791d876daf6c8f881746eee374
ruwanliyanage <ruwanliyanagegalle1994@gmail.com>
2019-05-31 17:51:31 +0530

    make routings

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,39 @@
 import { ExtraOptions, RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
-import { SignInComponent } from './components/sign-in/sign-in.component';
+import {
+  NbAuthComponent,
+  NbLoginComponent,
+  NbLogoutComponent,
+  NbRegisterComponent,
+  NbRequestPasswordComponent,
+  NbResetPasswordComponent
+} from '@nebular/auth';
 import { SecureInnerPagesGuard } from './shared/guard/secure-inner-pages.guard';
+import { SignInComponent } from './components/sign-in/sign-in.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
+import { VerifyEmailComponent } from './components/verify-email/verify-email.component';
+import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
 
 const routes: Routes = [
   { path: 'pages', loadChildren: 'app/pages/pages.module#PagesModule' },
   {
     path: 'sign-in',
     component: SignInComponent
+    //canActivate: [SecureInnerPagesGuard]
+  },
+  {
+    path: 'register-user',
+    component: SignUpComponent
+    //canActivate: [SecureInnerPagesGuard]
+  },
+  {
+    path: 'forgot-password',
+    component: ForgotPasswordComponent
+    //canActivate: [SecureInnerPagesGuard]
+  },
+  {
+    path: 'verify-email-address',
+    component: VerifyEmailComponent
     //canActivate: [SecureInnerPagesGuard]
   },
   { path: '', redirectTo: 'sign-in', pathMatch: 'full' },
