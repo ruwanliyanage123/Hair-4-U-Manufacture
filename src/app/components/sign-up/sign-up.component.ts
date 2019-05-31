5fbc0f34b0c7c7a1951006b729ece9cbedf9cb17
ruwanliyanage <ruwanliyanagegalle1994@gmail.com>
2019-05-31 08:08:16 +0530

    add css file

--- src/app/components/sign-up/sign-up.component.ts
+++ src/app/components/sign-up/sign-up.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'ngx-sign-up',
   templateUrl: './sign-up.component.html',
-  styleUrls: ['./sign-up.component.scss']
+  styleUrls: ['./sign-up.component.css']
 })
 export class SignUpComponent implements OnInit {
 
