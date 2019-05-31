23046ec1285649ee1ff8b9645033968801fe283b
ruwanliyanage <ruwanliyanagegalle1994@gmail.com>
2019-05-31 06:41:51 +0530

    add components/forgot-password

---
+++ src/app/components/forgot-password/forgot-password.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ForgotPasswordComponent } from './forgot-password.component';
+
+@NgModule({
+  declarations: [ForgotPasswordComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class ForgotPasswordModule { }
