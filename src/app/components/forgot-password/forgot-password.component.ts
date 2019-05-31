1145b7c31337cf66cf78c91f4ae02bf182e5895c
ruwanliyanage <ruwanliyanagegalle1994@gmail.com>
2019-05-31 15:27:53 +0530

    add css to registration

--- src/app/components/forgot-password/forgot-password.component.ts
+++ src/app/components/forgot-password/forgot-password.component.ts
@@ -4,7 +4,7 @@ import { AuthService } from '../../shared/services/auth.service';
 @Component({
   selector: 'ngx-forgot-password',
   templateUrl: './forgot-password.component.html',
-  styleUrls: ['./forgot-password.component.scss']
+  styleUrls: ['./forgot-password.component.css']
 })
 export class ForgotPasswordComponent implements OnInit {
   constructor(public authService: AuthService) {}
